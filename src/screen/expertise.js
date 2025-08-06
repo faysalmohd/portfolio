3a338cf7f189d7cd0df15a585fe464218ecd4b48
Faysal Mohammed <faysalmohammed2022@gmail.com>
2025-08-06 18:43:01 +0300

    done with expertise screen

--- src/screen/expertise.js
+++ src/screen/expertise.js
@@ -1,7 +1,69 @@
 import "./expertise.css";
+import SplitText from "../components/bounceText";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact, faJs, faHtml5, faCss3 } from "@fortawesome/free-brands-svg-icons";
 
-export default function Expertise({ children }) {
+export default function Expertise() {
+  const info = [ // remember to import the icon from the fa library
+   {
+    icon: faReact,
+    title: 'React',
+    description: 'nice react'
+   },
+   {
+    icon: faJs,
+    title: 'JavaScript',
+    description: 'nice js'
+   },
+   {
+    icon: faHtml5,
+    title: 'HTML',
+    description: 'nice html'
+   },
+   {
+    icon: faCss3,
+    title: 'CSS',
+    description: 'nice css'
+   }
+  ]
+
+  const Card = ({ icon, title = null, description = null }) => {
+    return (
+      <div className="card">
+        <div className="card-top">
+          <FontAwesomeIcon className="card-top-icon" icon={icon} />
+          <h3 className="card-top-title">{title}</h3>
+        </div>
+        <div className="card-bottom">
+          <p className="card-bottom-tag"></p>
+          <h3 className="card-bottom-description">{description}</h3>
+          <p className="card-bottom-endTag"></p>
+        </div>
+      </div>
+    );
+  };
+
   return (
-  <div className="expertiseDiv">hi</div>
-);
+    <div className="expertiseDiv">
+      <SplitText
+        text="MY EXPERTISE"
+        className="expertiseText"
+        delay={100}
+        duration={1}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+      />
+
+      <div className="card-section">
+        {info.map((ele) => {
+          return(
+            <Card icon={ele.icon} title={ele.title} description={ele.description}></Card>
+          )
+        })}
+      </div>
+    </div>
+  );
 }
--- src/screen/main.js
+++ src/screen/main.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaArrowDown } from "react-icons/fa";
 import "./main.css";
 import SplitText from "../components/bounceText";
